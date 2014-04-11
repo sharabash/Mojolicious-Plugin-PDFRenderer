d76fe8777c37a1b31fb7a0fe0984f630cefc5d72
Nour Sharabash <nour.sharabash@gmail.com>
2014-04-11 09:58:26 -0500

    init

---
+++ lib/Mojolicious/Plugin/PDFRenderer.pm
@@ -0,0 +1,37 @@
+package Mojolicious::Plugin::PDFRenderer;
+use Mojo::Base 'Mojolicious::Plugin';
+use Mojo::JSON qw/decode_json/;
+
+sub register {
+    my ( $self, $app, $opts ) = @_;
+    # writing it now...
+}
+
+# ABSTRACT: 
+1;
+
+=head1 SYNOPSIS
+
+    package App;
+    use Mojo::Base 'Mojolicious';
+
+    sub startup {
+        my $self = shift;
+
+        $self->plugin( 'Mojolicious::Plugin::PDFRenderer' );
+        # ...
+    }
+
+Then go to http://yourapp:3000/any/route, take a good look, then go to http://yourapp:3000/any/route.pdf. Cool, huh?
+
+=head1 REQUIREMENTS
+
+=over 2
+
+=item L<PDF::WebKit>
+
+=item L<"wkhtmltopdf"|http://wkhtmltopdf.org/>
+
+=back
+
+=cut
