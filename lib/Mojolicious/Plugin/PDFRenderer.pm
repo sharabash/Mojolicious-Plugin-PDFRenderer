499abb326bf2db71ef27595a1806d210aac2ebf6
Nour Sharabash <nour.sharabash@gmail.com>
2014-04-24 13:01:17 -0500

    commit

--- lib/Mojolicious/Plugin/PDFRenderer.pm
+++ lib/Mojolicious/Plugin/PDFRenderer.pm
@@ -6,7 +6,9 @@ use List::AllUtils qw/uniq/;
 sub register {
     my ( $self, $app, $opts ) = @_;
     $opts->{ '-args-override' } //= 1 unless exists $opts->{ '-args-override' };
+    $opts->{ '-conf-override' } //= 1 unless exists $opts->{ '-conf-override' };
     my $args_override = delete $opts->{ '-args-override' };
+    my $conf_override = delete $opts->{ '-conf-override' };
     my @opts = `wkhtmltopdf --extended-help`;
        @opts = map { my $opt = $_; $opt =~ s/.*--([^ =,]+).*/$1/g; $opt =~ s/-/_/g; $opt =~ s/\W//g; $opt =~ s/_+$//g; $opt } grep { $_ =~ /--[^ =]+/ } @opts;
        @opts = uniq @opts;
@@ -23,12 +25,55 @@ sub register {
             $app->log->debug( "...fetching url $url for pdf" );
 
             my %opts = %{ $opts };
-            do {
+
+            args_override: {
+                last args_override unless $args_override;
                 my %args = $c->args;
                 my @keys = grep { $okay{ $_ } } keys %args;
                 $opts{ $_ } = $args{ $_ } for @keys;
                 $app->log->debug( "pdf args override...", $app->dumper( \%opts ) ) if @keys;
-            } if $args_override;
+            };
+
+            conf_override: {
+                last conf_override unless $conf_override and exists $app->config->{pdf};
+                my ( %conf, %over, %seen );
+                my ( $path, @path );
+                %conf = %{ $app->config->{pdf} };
+                $path = $app->renderer->template_for( $c );
+                @path = split /\//, $path;
+                path: for my $i ( 0 .. $#path ) {
+                    my $conf = \%conf;
+                    my @path = @path[ 0 .. $i ];
+                    my @seen;
+                    seen: for my $path ( @path ) {
+                        last path unless exists $conf->{ $path }; $conf = $conf->{ $path };
+                        push @seen, $path; my $seen = join '/', @seen; next seen if $seen{ $seen }; $seen{ $seen } = 1;
+                        for my $key ( keys %{ $conf } ) {
+                            next if not defined $conf->{ $key } or ref $conf->{ $key };
+                            $over{ $key } = $conf->{ $key };
+                        }
+                    }
+                }
+                $path = $c->url_for;
+                $path =~ s/^\///;
+                @path = split /\//, $path;
+                path: for my $i ( 0 .. $#path ) {
+                    my $conf = \%conf;
+                    my @path = @path[ 0 .. $i ];
+                    my @seen;
+                    seen: for my $path ( @path ) {
+                        last path unless exists $conf->{ $path }; $conf = $conf->{ $path };
+                        push @seen, $path; my $seen = join '/', @seen; next seen if $seen{ $seen }; $seen{ $seen } = 1;
+                        for my $key ( keys %{ $conf } ) {
+                            next if not defined $conf->{ $key } or ref $conf->{ $key };
+                            $over{ $key } = $conf->{ $key };
+                        }
+                    }
+                }
+                $opts{ $_ } = $over{ $_ } for keys %over;
+                $app->log->debug( "pdf conf override...", $app->dumper( \%opts ) ) if %over;
+            };
+
             my $kit = new PDF::WebKit ( $url, %opts );
             my $pdf = $kit->to_pdf;
 
